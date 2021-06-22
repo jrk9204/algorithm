16eeef39567e52b101f56f778da9d79ce0b8ef84
Junrae Kim <jrk9204@gmail.com>
2021-06-23 01:09:03 +0900

    디스크 컨트롤러 시도

---
+++ 프로그래머스/디스크 컨트롤러.js
@@ -0,0 +1,8 @@
+//힙사용 문제
+// 힙에는 max heap, min heap 이 있는데 max heap은 root node 가 최고값, min은 root node 가 최소값 을 갖고있다.
+// 우선순위큐 === max heap, root node 가 우선 처리될 작업 1순위이다.
+//[[0,3],[1,9],[2,6]]
+
+let time = 0;
+let doneJob = 0;
+let answer = 0;
