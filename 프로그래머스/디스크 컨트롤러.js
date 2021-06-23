bc07f0bff9d9c9aee91622965b78ac88623f55ff
Junrae Kim <jrk9204@gmail.com>
2021-06-23 23:43:30 +0900

    디스크 컨트롤러 시도2

--- 프로그래머스/디스크 컨트롤러.js
+++ 프로그래머스/디스크 컨트롤러.js
@@ -3,6 +3,30 @@
 // 우선순위큐 === max heap, root node 가 우선 처리될 작업 1순위이다.
 //[[0,3],[1,9],[2,6]]
 
+
+//대기중인 큐
+let jobs = [[0, 3], [1, 9], [2, 6]];
+
 let time = 0;
 let doneJob = 0;
 let answer = 0;
+
+//우선순위 큐
+let priorityQueue = [jobs[0]];
+let jobNum = jobs.length;
+
+//모든 작업이 끝났을 경우
+// while(doneJob !== jobNum){
+
+//     if(time >=){
+//         priorityQueue.push(jobs[0]);
+//     }
+
+//     if(priorityQueue[0][0])
+
+
+// }
+
+
+console.log(priorityQueue)
+
