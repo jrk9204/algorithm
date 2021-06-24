1adf6d10f6c4b355d76b49a22227621f0c2123c7
Junrae Kim <jrk9204@gmail.com>
2021-06-24 23:35:14 +0900

    큐스택 문제추가

--- 프로그래머스/디스크 컨트롤러.js
+++ 프로그래머스/디스크 컨트롤러.js
@@ -3,30 +3,40 @@
 // 우선순위큐 === max heap, root node 가 우선 처리될 작업 1순위이다.
 //[[0,3],[1,9],[2,6]]
 
-
 //대기중인 큐
-let jobs = [[0, 3], [1, 9], [2, 6]];
+
+let jobs = [
+  [0, 3],
+  [1, 9],
+  [2, 6],
+];
+
+jobs.sort((a, b) => a[0] - b[0]);
 
 let time = 0;
-let doneJob = 0;
+let countJob = 0;
 let answer = 0;
 
 //우선순위 큐
-let priorityQueue = [jobs[0]];
-let jobNum = jobs.length;
+let priorityQueue = [];
 
 //모든 작업이 끝났을 경우
-// while(doneJob !== jobNum){
+while (jobs.length !== 0 || priorityQueue.length !== 0) {
+  if (jobs.length > countJob && time >= jobs[countJob][0]) {
+    if (priorityQueue.length === 0) time = jobs[countJob][0];
 
-//     if(time >=){
-//         priorityQueue.push(jobs[0]);
-//     }
+    priorityQueue.push(jobs[countJob]);
+    priorityQueue.sort((a, b) => a[1] - b[1]);
+    jobs.shift();
+  } else if (priorityQueue.length !== 0) {
+    time += priorityQueue[0][1];
+    answer += time - priorityQueue[0][0];
+    priorityQueue.shift();
+  }
 
-//     if(priorityQueue[0][0])
+  countJob += 1;
+}
 
-
-// }
-
-
-console.log(priorityQueue)
-
+// time += priorityQueue[0][1];
+// answer += time - priorityQueue[0][0];
+console.log(answer);
