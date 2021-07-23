b225ce04a8b9c1557ad4c13b935e91396596a0d1
Junrae Kim <kim34@cougars.csusm.edu>
2021-07-23 17:36:12 +0900

    스킬트리 문제풀이

--- 프로그래머스/스킬트리.js
+++ 프로그래머스/스킬트리.js
@@ -1,8 +1,8 @@
 function solution(skill, skill_trees) {
   let splitOrigin = skill.split("");
   let searchSkill = skill_trees.map((el) => el.split(""));
-
-  let findSkil = searchSkill.map((el) => {
+  let answer = 0;
+  let findSkill = searchSkill.map((el) => {
     return el.filter((target) => {
       if (splitOrigin.indexOf(target) !== -1) {
         return target;
@@ -10,19 +10,15 @@ function solution(skill, skill_trees) {
     });
   });
 
-  console.log(findSkil);
-
-  let answer = 0;
-  searchSkill.forEach((el, idx) => {
-    let searching = true;
-
-    el.forEach((searchEl, searchIdx) => {
-      if (el !== splitOrigin[searchIdx]) {
-        searching = false;
+  findSkill.forEach((searchEl, searchIdx) => {
+    let findit = false;
+    searchEl.forEach((targetEl, targetIdx) => {
+      if (splitOrigin[targetIdx] !== searchEl[targetIdx]) {
+        findit = true;
       }
     });
 
-    if (searching === true) {
+    if (!findit) {
       answer += 1;
     }
   });
